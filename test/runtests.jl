e9e99c1d1c95b3e9d7d16e7ea4a5ad3e099e6a8d
Christian Merdon <merdon@edu-032.wias-berlin.de>
2019-11-12 15:16:56 +0100

    added tests for quadrature formulas (up to degree 20)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,16 @@
 using Test
-
 push!(LOAD_PATH, "../src")
-using P1approx
 
+using Quadrature
+println("\nStarting tests for Quadrature")
+for order = 1 : 20
+    println("Testing order ", order);
+    @test Quadrature.QuadratureTest(order)
+end
+
+
+using P1approx
 println("Starting tests for P1approx")
 @test P1approx.P1Test1()
 @test P1approx.P1Test2()
+
