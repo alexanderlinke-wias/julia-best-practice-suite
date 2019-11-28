158313e0c931654934970f223f39396f6cc5fae5
Christian Merdon <merdon@edu-109.wias-berlin.de>
2019-11-28 17:11:00 +0100

    P1 basis functions are now defined in closures of one top-level function

--- src/FiniteElements.jl
+++ src/FiniteElements.jl
@@ -17,25 +17,36 @@ struct FiniteElement{T <: Real}
     local_mass_matrix::Array{T,2};
 end
 
-function bary1_ref(xref)
-    return xref[1];
-end    
+function bary(j::Int)
+    function closure(xref)
+        return xref[j];
+    end    
+end
 
-function bary2_ref(xref)
-    return xref[2];
-end    
+function P1FEFunctions1D(j)
+    function closure(x,grid::Grid.Mesh,cell)
+        return sqrt(sum((grid.coords4nodes[grid.nodes4cells[cell,j],:] - x).^2)) / grid.volume4cells[cell];        
+    end
+end
 
-function bary3_ref(xref)
-    return xref[3];
-end  
-
-function line_bary1(x,grid::Grid.Mesh,cell)
-    return sqrt(sum((grid.coords4nodes[grid.nodes4cells[cell,2],:] - x).^2)) / grid.volume4cells[cell];
-end   
-
-function line_bary2(x,grid::Grid.Mesh,cell)
-    return sqrt(sum((grid.coords4nodes[grid.nodes4cells[cell,1],:] - x).^2)) / grid.volume4cells[cell];
-end   
+function P1FEFunctions2D(j)
+    if (j == 3)
+        index1 = 1;
+    else
+        index1 = j + 1;
+    end    
+    if (j == 1)
+        index2 = 3;
+    else
+        index2 = j - 1;
+    end   
+    function closure(x,grid::Grid.Mesh,cell)
+        return (grid.coords4nodes[grid.nodes4cells[cell,index1],1]*grid.coords4nodes[grid.nodes4cells[cell,index2],2]
+    - grid.coords4nodes[grid.nodes4cells[cell,index2],1]*grid.coords4nodes[grid.nodes4cells[cell,index2],2]
+    + x[1]*(grid.coords4nodes[grid.nodes4cells[cell,index1],2] - grid.coords4nodes[grid.nodes4cells[cell,index2],2])
+    + x[2]*(grid.coords4nodes[grid.nodes4cells[cell,index2],1] - grid.coords4nodes[grid.nodes4cells[cell,index1],1]))/(2*grid.volume4cells[cell])
+    end
+end
 
 function line_bary1_grad!(result,x,grid,cell)
     @assert size(grid.coords4nodes,2) == 1 # todo: implement exact gradient for higher dimension line segments
@@ -46,30 +57,6 @@ function line_bary2_grad!(result,x,grid,cell)
     @assert size(grid.coords4nodes,2) == 1 # todo: implement exact gradient for higher dimension line segments
     result[1] = 1 / grid.volume4cells[cell];
 end   
-
-function triangle_bary1(x,grid::Grid.Mesh,cell)
-    return (grid.coords4nodes[grid.nodes4cells[cell,2],1]*grid.coords4nodes[grid.nodes4cells[cell,3],2]
-    - grid.coords4nodes[grid.nodes4cells[cell,3],1]*grid.coords4nodes[grid.nodes4cells[cell,2],2]
-    + x[1]*(grid.coords4nodes[grid.nodes4cells[cell,2],2] - grid.coords4nodes[grid.nodes4cells[cell,3],2])
-    + x[2]*(grid.coords4nodes[grid.nodes4cells[cell,3],1] - grid.coords4nodes[grid.nodes4cells[cell,2],1]))/(2*grid.volume4cells[cell])
-end    
-
-
-function triangle_bary2(x,grid::Grid.Mesh,cell)
-    return (-grid.coords4nodes[grid.nodes4cells[cell,1],1]*grid.coords4nodes[grid.nodes4cells[cell,3],2]
-    - grid.coords4nodes[grid.nodes4cells[cell,3],1]*grid.coords4nodes[grid.nodes4cells[cell,1],2]
-    + x[1]*(grid.coords4nodes[grid.nodes4cells[cell,3],2] - grid.coords4nodes[grid.nodes4cells[cell,1],2])
-    + x[2]*(grid.coords4nodes[grid.nodes4cells[cell,1],1] - grid.coords4nodes[grid.nodes4cells[cell,3],1]))/(2*grid.volume4cells[cell])
-end    
-
-
-function triangle_bary3(x,grid::Grid.Mesh,cell)
-    return (grid.coords4nodes[grid.nodes4cells[cell,1],1]*grid.coords4nodes[grid.nodes4cells[cell,2],2]
-    - grid.coords4nodes[grid.nodes4cells[cell,1],1]*grid.coords4nodes[grid.nodes4cells[cell,2],2]
-    + x[1]*(grid.coords4nodes[grid.nodes4cells[cell,1],2] - grid.coords4nodes[grid.nodes4cells[cell,2],2])
-    + x[2]*(grid.coords4nodes[grid.nodes4cells[cell,2],1] - grid.coords4nodes[grid.nodes4cells[cell,1],1]))/(2*grid.volume4cells[cell])
-end    
-
 
 function triangle_bary1_grad!(result,x,grid,cell)
     result[1] = grid.coords4nodes[grid.nodes4cells[cell,2],2] - grid.coords4nodes[grid.nodes4cells[cell,3],2];
@@ -90,31 +77,14 @@ function triangle_bary3_grad!(result,x,grid,cell)
 end
 
 
-function FDgradient!(result,x,grid,cell,bfun::Function)
-    f(x) = bfun(x,grid,cell);
-    g = x -> ForwardDiff.gradient(f,x);
-    result[:] = g(x);
+function FDgradient!(bfun::Function)
+    function closure(result,x,grid,cell)
+        f(x) = bfun(x,grid,cell);
+        g = x -> ForwardDiff.gradient(f,x);
+        result[:] = g(x);
+    end    
 end    
 
-function triangle_bary1_FDgrad!(result,x,grid,cell)
-    FDgradient!(result,x,grid,cell,triangle_bary1);
-end
-
-function triangle_bary2_FDgrad!(result,x,grid,cell)
-    FDgradient!(result,x,grid,cell,triangle_bary2);
-end
-
-function triangle_bary3_FDgrad!(result,x,grid,cell)
-    FDgradient!(result,x,grid,cell,triangle_bary3);
-end
-
-function line_bary1_FDgrad!(result,x,grid,cell)
-    FDgradient!(result,x,grid,cell,line_bary1);
-end
-
-function line_bary2_FDgrad!(result,x,grid,cell)
-    FDgradient!(result,x,grid,cell,line_bary2);
-end
 
 function get_P1FiniteElementFD(grid::Grid.Mesh)
     T = eltype(grid.coords4nodes)
@@ -123,17 +93,28 @@ function get_P1FiniteElementFD(grid::Grid.Mesh)
     ensure_volume4cells!(grid);
     dofs4faces = grid.nodes4faces;
     coords4dof = grid.coords4nodes;
+    println("Initialising FiniteElement with ForwardDiff gradients...");
+    
     
     # group basis functions
     celldim = size(grid.nodes4cells,2);
     if celldim == 3 # triangles
-        bfun_ref = [bary1_ref,bary2_ref,bary3_ref];
-        bfun = [triangle_bary1,triangle_bary2,triangle_bary3];
-        bfun_grad! = [triangle_bary1_FDgrad!,triangle_bary2_FDgrad!,triangle_bary3_FDgrad!];
+        bfun_ref = [bary(1),
+                    bary(2),
+                    bary(3)];
+        bfun = [P1FEFunctions2D(1),
+                P1FEFunctions2D(2),
+                P1FEFunctions2D(3)];
+        bfun_grad! = [FDgradient!(P1FEFunctions2D(1)),
+                      FDgradient!(P1FEFunctions2D(2)),
+                      FDgradient!(P1FEFunctions2D(3))];
     elseif celldim == 2 # line segments
-        bfun_ref = [bary1_ref,bary2_ref];
-        bfun = [line_bary1,line_bary2];
-        bfun_grad! = [line_bary1_FDgrad!,line_bary2_FDgrad!];
+        bfun_ref = [bary(1),
+                    bary(2)];
+        bfun = [P1FEFunctions1D(1),
+                P1FEFunctions1D(2)];
+        bfun_grad! = [FDgradient!(P1FEFunctions1D(1)),
+                      FDgradient!(P1FEFunctions1D(2))];
     end    
     
     local_mass_matrix = (ones(T,celldim,celldim) + LinearAlgebra.I(celldim)) * 1 // ((celldim)*(celldim+1));
@@ -147,18 +128,28 @@ function get_P1FiniteElement(grid::Grid.Mesh)
     dofs4faces = grid.nodes4faces;
     coords4dof = grid.coords4nodes;
     ensure_volume4cells!(grid);
+    println("Initialising FiniteElement with exact gradients...");
     
     
     # group basis functions
     celldim = size(grid.nodes4cells,2);
     if celldim == 3 # triangles
-        bfun_ref = [bary1_ref,bary2_ref,bary3_ref];
-        bfun = [triangle_bary1,triangle_bary2,triangle_bary3];
-        bfun_grad! = [triangle_bary1_grad!,triangle_bary2_grad!,triangle_bary3_grad!];
+        bfun_ref = [bary(1),
+                    bary(2),
+                    bary(3)];
+        bfun = [P1FEFunctions2D(1),
+                P1FEFunctions2D(2),
+                P1FEFunctions2D(3)];
+        bfun_grad! = [triangle_bary1_grad!,
+                      triangle_bary2_grad!,
+                      triangle_bary3_grad!];
     elseif celldim == 2 # line segments
-        bfun_ref = [bary1_ref,bary2_ref];
-        bfun = [line_bary1,line_bary2];
-        bfun_grad! = [line_bary1_grad!,line_bary2_grad!];
+        bfun_ref = [bary(1),
+                    bary(2)];
+        bfun = [P1FEFunctions1D(1),
+                P1FEFunctions1D(2)];
+        bfun_grad! = [line_bary1_grad!,
+                      line_bary2_grad!];
     end    
     
     local_mass_matrix = (ones(T,celldim,celldim) + LinearAlgebra.I(celldim)) * 1 // ((celldim)*(celldim+1));
